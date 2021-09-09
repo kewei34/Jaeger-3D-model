532d563efc8ccd8de0f1f3eac9d7731615a1aa3e
Jeriel Ng <ngjeriel59@gmail.com>
2021-09-09 23:27:20 +0800

    9/9/2021 11:26pm

--- robot.cpp
+++ robot.cpp
@@ -1,6 +1,8 @@
-
 #include <Windows.h>
+#include <Windowsx.h>
+#include <math.h>
 #include <gl/GL.h>
+#include <gl/GLU.h>
 
 #include "base.cpp"
 #include "scene.cpp"
@@ -10,6 +12,30 @@
 
 #define WINDOW_TITLE "OpenGL Window"
 
+GLuint LoadBMP(char* fileName);
+
+//draw curve line
+GLfloat ctrlpoints[3][3] = {
+	{ 6, 4, 0 },{ 4, -2, 0 },{ 0, 1.5, 0 }
+};
+
+GLfloat ctrlpoints2[3][3] = {
+	{ 6, 4, 0 },{ 4, -2, 0 },{ 0, 1.5, 0 }
+};
+
+// mouse movement
+float lastX = 0.0f, lastY = 0.0f;
+
+float xRotated = 1.0f, yRotated = 1.0f, zRotated = -30.0f;
+
+
+float x = 0.0f, y = 0.0f, z = 7.0f;
+float zoomLevel = -7.0f;
+
+float xPosition = 0.0f, yPosition = 0.0f, zPosition = 0.05f;
+
+BITMAP BMP;
+HBITMAP hBMP = NULL;
 
 LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -18,11 +44,24 @@ LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
-
+	case WM_MOUSEMOVE:
+		switch (wParam) {
+		case MK_LBUTTON:
+			int xPos = GET_X_LPARAM(lParam);
+			int yPos = GET_Y_LPARAM(lParam);
+			zRotated += xPos - lastX;
+			xRotated += yPos - lastY;
+			lastX = xPos;
+			lastY = yPos;
+			break;
+		}
+		break;	
 	case WM_KEYDOWN:
 		if (wParam == VK_ESCAPE) PostQuitMessage(0);
 		break;
-
+	case WM_MOUSEWHEEL:
+		zoomLevel += GET_WHEEL_DELTA_WPARAM(wParam) / 120.0f;
+		break;
 	default:
 		break;
 	}
@@ -62,6 +101,44 @@ bool initPixelFormat(HDC hdc)
 	}
 }
 //--------------------------------------------------------------------
+GLuint LoadBMP(char* fileName) {
+	//glColor3f(1.0f, 1.0f, 1.0f);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+	hBMP = (HBITMAP)LoadImage(GetModuleHandle(NULL), fileName, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_LOADFROMFILE);
+	GetObject(hBMP, sizeof(BMP), &BMP);
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, BMP.bmWidth, BMP.bmHeight, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, BMP.bmBits);
+
+	DeleteObject(hBMP);
+	return texture;
+}
+
+GLuint LoadBMPForReactor(char* fileName) {
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+	hBMP = (HBITMAP)LoadImage(GetModuleHandle(NULL), fileName, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_LOADFROMFILE);
+	GetObject(hBMP, sizeof(BMP), &BMP);
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, BMP.bmWidth, BMP.bmHeight, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, BMP.bmBits);
+
+	DeleteObject(hBMP);
+	return texture;
+}
+
 
 void display()
 {
@@ -110,6 +187,19 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 	MSG msg;
 	ZeroMemory(&msg, sizeof(msg));
 
+	//perspective
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(70.0, 1.0, 0.6, 21.0);
+	glEnable(GL_DEPTH_TEST);
+	//glEnable(GL_STENCIL_TEST);
+	//glStencilOp(GL_KEEP, GL_KEEP, GL_INCR);
+	//glStencilFunc(GL_ALWAYS, 0, 1); // these are also the default parameters
+	glMap1f(GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 3, &ctrlpoints[0][0]);
+	glEnable(GL_MAP1_VERTEX_3);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_TEXTURE_2D);
+
 	while (true)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
@@ -125,6 +215,10 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 		SwapBuffers(hdc);
 	}
 
+	glDisable(GL_TEXTURE_2D);
+	DeleteObject(hBMP);
+	glDisable(GL_LIGHTING);
+	//glDisable(GL_STENCIL_TEST);
 	UnregisterClass(WINDOW_TITLE, wc.hInstance);
 
 	return true;
