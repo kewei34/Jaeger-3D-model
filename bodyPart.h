e1ad735b7866dc47181c22bc99cf3660ee376c87
Ke Wei <kewei2001@hotmail.com>
2021-09-11 00:34:32 +0800

    robot finger

--- bodyPart.h
+++ bodyPart.h
@@ -1,3 +1,15 @@
 #pragma once
 
 void bone(float boneLength);
+
+void joint(float boneLength,float boneWidth);
+
+void finger(float length);
+
+void lowerFingers();
+
+void upperFingers(float length);
+
+void fingerJoint();
+
+void allFingers();
